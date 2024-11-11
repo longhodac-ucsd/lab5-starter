352c046338b11b0f12b03d752de59216cf11c34a
longhodac-ucsd <lhodac@ucsd.edu>
2024-11-11 05:51:30 +0000

    afads

--- number-server.c
+++ number-server.c
@@ -4,17 +4,34 @@
 int num = 0;
 
 char const HTTP_404_NOT_FOUND[] = "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\n\r\n";
+char const HTTP_202_OK[] = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n";
 
 void handle_404(int client_sock, char *path)  {
     printf("SERVER LOG: Got request for unrecognized path \"%s\"\n", path);
-
+    const char* headerNotFound = "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\n\r\n";
+    
     char response_buff[BUFFER_SIZE];
     snprintf(response_buff, BUFFER_SIZE, "Error 404:\r\nUnrecognized path \"%s\"\r\n", path);
     // snprintf includes a null-terminator
 
     // TODO: send response back to client?
+    write(client_sock, headerNotFound, strlen(headerNotFound));
+    write(client_sock, response_buff, strlen(response_buff));
 }
 
+void handle_root(int client_sock){
+    char message[] = "Got http 200 ok response, current number to come";
+    write(client_sock, HTTP_202_OK, strlen(HTTP_202_OK));
+    write(client_sock, message, strlen(message));
+}
+
+void handle_increment(int client_sock){
+    char message[BUFFER_SIZE];
+    num += 1;
+    snprintf(message, BUFFER_SIZE, "Number incremented to: %dd\n", num);
+    write(client_sock, HTTP_202_OK, strlen(HTTP_202_OK));
+    write(client_sock, message, strlen(message));
+}
 
 void handle_response(char *request, int client_sock) {
     char path[256];
@@ -27,7 +44,21 @@ void handle_response(char *request, int client_sock) {
         return;
     }
 
-    handle_404(client_sock, path);
+    if (strcmp(path, "/") == 0){
+        handle_root(client_sock);
+    }
+    else if (strcmp(path, "/increment") == 0){
+        handle_increment(client_sock);
+        return;
+    }
+    else if (strcmp(path, "/add") == 0){
+        printf("Handling add %s\n", path);
+        return;
+    }
+    else{
+        handle_404(client_sock, path);
+    }
+    
 }
 
 int main(int argc, char *argv[]) {
